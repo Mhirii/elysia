cfa31cc1a70bc4b64e51c16ab8b2d7704aa1fee2
Mhirii <ahmedmhiri218@gmail.com>
2023-12-31 14:32:44 +0100

    init

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get("/", () => "hi")
+  .get("/id/:id", ({ params: { id } }) => id)
+  .post("/post", ({ body }) => body)
+  .listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
